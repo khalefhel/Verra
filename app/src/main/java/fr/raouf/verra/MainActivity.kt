504b89285065363d72b55f68ef72738342a49198
unknown <RaoufBensaid>
2024-01-05 17:12:56 +0100

    Update the interaction between login and registration

--- app/src/main/java/fr/raouf/verra/MainActivity.kt
+++ app/src/main/java/fr/raouf/verra/MainActivity.kt
@@ -1,13 +1,20 @@
 package fr.raouf.verra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fr.raouf.verra.fragments.LinkFragment
+import android.view.View
+import fr.raouf.verra.fragments.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login
-        )
+        setContentView(R.layout.login)
+
+        val button_register: View = findViewById(R.id.button_register)
+        button_register.setOnClickListener {
+            val intent = Intent(this, RegisterFragment::class.java)
+            startActivity(intent)
+        }
     }
 }
