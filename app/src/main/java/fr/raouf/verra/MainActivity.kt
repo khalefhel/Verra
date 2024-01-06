376e47dfe87ec2fbc278ee0986ea9841aa7ba1ad
unknown <RaoufBensaid>
2024-01-06 05:29:25 +0100

    create home

--- app/src/main/java/fr/raouf/verra/MainActivity.kt
+++ app/src/main/java/fr/raouf/verra/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import fr.raouf.verra.fragments.HomeFragment
 import fr.raouf.verra.fragments.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         val button_register: View = findViewById(R.id.button_register)
         button_register.setOnClickListener {
             val intent = Intent(this, RegisterFragment::class.java)
+            startActivity(intent)
+        }
+
+        val button_tst: View = findViewById(R.id.button_tst)
+        button_tst.setOnClickListener {
+            val intent = Intent(this, HomeFragment::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/fr/raouf/verra/fragments/HomeFragment.kt
+++ app/src/main/java/fr/raouf/verra/fragments/HomeFragment.kt
@@ -1,4 +1,14 @@
 package fr.raouf.verra.fragments
 
-class HomeFragment {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fr.raouf.verra.R
+
+class HomeFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_page)
+    }
+
 }
