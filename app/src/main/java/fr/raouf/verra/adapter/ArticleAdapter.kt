bb1c0e5126ac20a5ef28364a09cf751ec8b2b735
unknown <RaoufBensaid>
2024-01-07 04:39:33 +0100

    VERRA

--- app/src/main/java/fr/raouf/verra/adapter/ArticleAdapter.kt
+++ app/src/main/java/fr/raouf/verra/adapter/ArticleAdapter.kt
@@ -1,4 +1,30 @@
 package fr.raouf.verra.adapter
 
-class ArticleAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.raouf.verra.R
+
+class ArticleAdapter : RecyclerView.Adapter<ArticleAdapter.ViewHolder>(){
+
+    // Boite pour ranger tout les composants à controler
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val articleImage = view.findViewById<ImageView>(R.id.article_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.article_horizontal, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 5
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
 }
